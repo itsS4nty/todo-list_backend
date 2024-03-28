9f609eab582c8feaebfcb0c6d4004772dc141b46
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2024-03-28 19:21:20 +0100

    fix: schemas test

--- tests/lib/schemas.spec.ts
+++ tests/lib/schemas.spec.ts
@@ -1,27 +1,5 @@
-import { ZodIssue } from 'zod';
 import { validateSchemaSafe, ZodSchema } from '../../src/lib/schemas/index';
-import { createDutySchema } from '../../src/lib/schemas/models/createDutySchema';
-import { deleteDutySchema } from '../../src/lib/schemas/models/deleteDutySchema';
-import { findDutiesSchema } from '../../src/lib/schemas/models/findDutiesSchema';
-import { updateDutySchema } from '../../src/lib/schemas/models/updateDutySchema';
 import { DutyStatus } from '../../src/enums/dutyStatus';
-
-jest.mock('../../src/lib/schemas/models/createDutySchema', () => ({
-    createDutySchema: { safeParse: jest.fn() },
-}));
-jest.mock('../../src/lib/schemas/models/deleteDutySchema', () => ({
-    deleteDutySchema: { safeParse: jest.fn() },
-}));
-jest.mock('../../src/lib/schemas/models/findDutiesSchema', () => ({
-    findDutiesSchema: { safeParse: jest.fn() },
-}));
-jest.mock('../../src/lib/schemas/models/updateDutySchema', () => ({
-    updateDutySchema: { safeParse: jest.fn() },
-}));
-
-const createMockZodIssue = (message: string): ZodIssue[] => [
-    { message, path: [], code: 'custom', params: {} },
-];
 
 describe('Schema Validation', () => {
     beforeEach(() => {
@@ -30,28 +8,22 @@ describe('Schema Validation', () => {
 
     describe('validateSchemaSafe', () => {
         it.each([
-            ['CreateDuty', createDutySchema, { name: 'New Duty' }],
-            ['DeleteDuty', deleteDutySchema, { id: 1, fullDelete: false }],
-            ['FindDuties', findDutiesSchema, { status: DutyStatus.PENDING }],
-            [
-                'UpdateDuty',
-                updateDutySchema,
-                { id: 1, name: 'Updated Duty', status: DutyStatus.DONE },
-            ],
-        ])('validates %s schema correctly', async (schemaId, schema, data) => {
+            ['CreateDuty', { name: 'New Duty' }],
+            ['DeleteDuty', { id: 1, fullDelete: false }],
+            ['FindDuties', { status: DutyStatus.PENDING }],
+            ['UpdateDuty', { id: 1, name: 'Updated Duty', status: DutyStatus.DONE }],
+        ])('validates %s schema correctly', async (schemaId, data) => {
             // (schema.safeParse as jest.Mock).mockReturnValue({ success: true, data });
-
             const result = validateSchemaSafe(schemaId as ZodSchema, data);
             expect(result).toEqual(data);
-            expect(schema.safeParse).toHaveBeenCalledWith(data);
         });
 
         it.each([
-            ['CreateDuty', createDutySchema, { name: '' }],
-            ['DeleteDuty', deleteDutySchema, { id: 0, fullDelete: null }],
-            ['FindDuties', findDutiesSchema, { status: undefined }],
-            ['UpdateDuty', updateDutySchema, { id: -1, name: '', status: null }],
-        ])('returns null for invalid %s data', (schemaId, schema, data) => {
+            ['CreateDuty', { name: 1 }],
+            ['DeleteDuty', { id: 0, fullDelete: null }],
+            ['FindDuties', { status: undefined }],
+            ['UpdateDuty', { id: -1, name: '', status: null }],
+        ])('returns null for invalid %s data', (schemaId, data) => {
             // (schema.safeParse as jest.Mock).mockReturnValue({
             //     success: false,
             //     error: { issues: createMockZodIssue('Invalid input') },
@@ -59,7 +31,6 @@ describe('Schema Validation', () => {
 
             const result = validateSchemaSafe(schemaId as ZodSchema, data);
             expect(result).toBeNull();
-            expect(schema.safeParse).toHaveBeenCalledWith(data);
         });
     });
 });
